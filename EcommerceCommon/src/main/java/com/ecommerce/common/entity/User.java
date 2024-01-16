dc1b240d842d4c323e3e9752028350b131618c1f
Trần Mạnh Hưng <tranhung26122612@gmail.com>
2024-01-16 12:06:29 +0700

    Import sample user data

--- EcommerceCommon/src/main/java/com/ecommerce/common/entity/User.java
+++ EcommerceCommon/src/main/java/com/ecommerce/common/entity/User.java
@@ -38,7 +38,7 @@ public class User {
 
     @ManyToMany
     @JoinTable(
-            name = "user_roles",
+            name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
